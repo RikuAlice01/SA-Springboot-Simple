ff1cb4e9a39c57f631f2ff2e2ab09af7044fde21
virusman1 <xirb_01@hotmail.co.th>
2018-09-05 22:43:23 +0700

    3rd Add Comment tag

--- src/main/java/com/sut/sa/cpe/CpeApplication.java
+++ src/main/java/com/sut/sa/cpe/CpeApplication.java
@@ -20,34 +20,32 @@ public class CpeApplication {
     @Bean
     ApplicationRunner init(UserRepository userrepository,CommentRepository commentrepository,VideoRepository videorepository) {
         return args -> {
-            Stream.of("Tanapon","Sitthichai","Somchai").forEach(name -> {
-                User user = new User();
-                Video video = new Video();
-                Comment comment = new Comment();
-                String code = new String();
-                code = "O8etN-2fc1c";
-                user.setName(name);
-                userrepository.save(user);
+            Stream.of("Tanapon","Sitthichai","Somchai").forEach(name -> {       //Stream "ชื่อ" ผ่านตัวแปร name โดยวนลูปตามจำนวนข้อมูล
+                User user = new User();                                         //สร้าง Objcet User
+                Video video = new Video();                                      //สร้าง Object Video
+                Comment comment = new Comment();                                //สร้าง Object Comment
+                String code = "O8etN-2fc1c";                                    //สร้าง ตัวแปรประเภท String และกำหนดค่า
+                user.setName(name);                                             //set name บน Object ชื่อ user
+                userrepository.save(user);                                      //บันทึก Objcet ชื่อ user
 
                 if (name == "Sitthichai") {
-                    video.setCode(code);
-                    video.setUrl("https://www.youtube.com/watch?v=" + code);
-                    video.setTitle("Tao Kae Noi Presents BNK48 1st Concert \"STARTO\"");
-                    video.setUserID(user.getUserID(name));
-                    videorepository.save(video);
-                    
-                    videorepository.findAll().forEach(System.out::println);
+                    video.setCode(code);                                        //set code บน Object ชื่อ video
+                    video.setUrl("https://www.youtube.com/watch?v=" + code);    //set code บน Object ชื่อ video
+                    video.setTitle("Tao Kae Noi Presents BNK48 1st Concert \"STARTO\""); //set code บน Object ชื่อ video
+                    video.setUserID(user.getUserID(name));                      //get User Id เพื่อ set User Id บน Object ชื่อ video
+                    videorepository.save(video);                                //บันทึก Objcet ชื่อ video
+                    videorepository.findAll().forEach(System.out::println);     // แสดง ข้อมูลทั้งหมดใน Entity video บน Terminal
 
-                    comment.setContent("The first comment.");
-                    comment.setUserID(user.getUserID(name));
-                    comment.setVideoID(video.getVideoID(code));
-                    commentrepository.save(comment);
+                    comment.setContent("The first comment.");                   //set content บน Object ชื่อ comment
+                    comment.setUserID(user.getUserID(name));                    //get User Id เพื่อ set User Id บน Object ชื่อ comment
+                    comment.setVideoID(video.getVideoID(code));                 //get Video Id เพื่อ set Video Id บน Object ชื่อ comment
+                    commentrepository.save(comment);                            //บันทึก Objcet ชื่อ comment
                     
-                    commentrepository.findAll().forEach(System.out::println);
+                    commentrepository.findAll().forEach(System.out::println);   // แสดง ข้อมูลทั้งหมดใน Entity comment บน Terminal
                 }
                 
             });
-            userrepository.findAll().forEach(System.out::println);
+            userrepository.findAll().forEach(System.out::println);  // แสดง ข้อมูลทั้งหมดใน Entity user บน Terminal
         };
     }
 
